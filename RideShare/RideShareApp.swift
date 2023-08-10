431a38350f809a229554c089d3fe666487497118
YMA98 <myanqi98@gmail.com>
2023-08-09 19:14:56 -0500

    Initial Commit

---
+++ RideShare/RideShareApp.swift
@@ -0,0 +1,17 @@
+//
+//  RideShareApp.swift
+//  RideShare
+//
+//  Created by Rocky Ma on 2023/8/9.
+//
+
+import SwiftUI
+
+@main
+struct RideShareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
